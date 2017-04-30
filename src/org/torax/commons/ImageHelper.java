aee8d3fdaed78c3665aae2d7796add4da8de9453
Jouwee <nicolaspohren@gmail.com>
2017-04-30 17:59:50 -0300

    Copied class ImageHelper from original project into commons

---
+++ src/org/torax/commons/ImageHelper.java
@@ -0,0 +1,31 @@
+package org.torax.commons;
+
+/**
+ * Helper class to work with images
+ */
+public class ImageHelper {
+    
+    /**
+     * Creates a image representation
+     * 
+     * @param data Image data
+     * @param range Image range
+     * @return Image
+     */
+    public static Image create(int[][] data, Range<Integer> range) {
+        int[][][] dataImage = new int[1][data.length][data[0].length];
+        dataImage[0] = data;
+        return new Image(dataImage, range);
+    }
+
+    /**
+     * Returns the image data
+     * 
+     * @param image Image representation
+     * @return int[][]
+     */
+    public static int[][] getData(Image image) {
+        return image.getData()[0];
+    }
+
+}
